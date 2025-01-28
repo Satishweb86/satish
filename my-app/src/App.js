8289cd07997d95f7beb7b3dac6543b20042430ef
Satishweb86 <skpro551@gmail.com>
2025-01-28 16:10:31 +0530

    first commit

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const rat = () => {
+  return (
+    <div>
+      
+      <button></button>1
+    </div>
+  )
+}
+
+export default rat;
+
+
---
+++ my-app/src/tic.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import rat from "./App.js"
+
+const tic = () => {
+  return (
+    <div className='container'>
+        <div className='sat'>
+           <rat/>
+           <rat/>
+           <okk/>
+        </div>  
+        <div className='sat'>
+           <rat/>
+           <rat/>
+           <ok/>
+        </div>   
+        <div className='sat'>
+           <ok/>
+           <ok/>
+           <ok/>
+        </div>
+                                                                                                        
+
+      
+    </div>
+  )
+}
+
+export default tic
